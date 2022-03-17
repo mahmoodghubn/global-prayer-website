8bb0b8d22df114814daaafbf40afc3701d81e231
mahmood.ghubn@gmail.com <mahmood.ghubn@gmail.com>
2022-03-17 18:55:33 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Global Prayer </h1>
+        <h2>Privacy Policy</h2>
         <p>
           We don't collect any of your data. Any permission we ask for is needed
           for the services provided by this app.
